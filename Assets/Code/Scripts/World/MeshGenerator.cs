fb91f373a098020d84e1d6e36f89991816957fec
Ole Kristian Elnæs <ole.elnaes@gmail.com>
2022-09-17 15:23:46 +0200

    Procedural generation: added terrain colors, height curve modification, other minor changes

--- Assets/Code/Scripts/World/MeshGenerator.cs
+++ Assets/Code/Scripts/World/MeshGenerator.cs
@@ -9,9 +9,14 @@ public class MeshGenerator : MonoBehaviour
     
     Vector3[] vertices;
     int[] triangles;
+    Color[] colors;
 
-    public int xSize = 150;
-    public int zSize = 150;
+    public int seed = 10000;
+    public int xSize = 255;
+    public int zSize = 255;
+
+    public AnimationCurve meshHeightCurve;
+    public int heightScale = 30;
 
     
     void Start()
@@ -19,24 +24,67 @@ public class MeshGenerator : MonoBehaviour
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
-        CreateShape();
+        CreateMesh();
         UpdateMesh();
     }
 
-   void CreateShape() 
+   void CreateMesh() 
    {
+
+        //----------------------------------------IMRPROVEMENT_TODO----------------------------------------
+        //TODO: A LOT of performance improvements can be done with the code below (between the lines)
+
         vertices = new Vector3[(xSize + 1) * (zSize + 1)];
         
-        for(int i = 0, z = 0; z <= zSize; z++)
+        for (int i = 0, z = 0; z <= zSize; z++)
         {
             for (int x = 0; x <= xSize; x++)
             {
                 Vector2 offset = new Vector2(0, 0);
-                float y = generateNoiseValue(x, z, 2.0f, 5, 1.8f, 0.6f, 1222, offset);
+                float y = generateNoiseValue(x, z, 1.0f, 7, 1.8f, 0.4f, seed, offset);
                 vertices[i] = new Vector3(x, y, z);
                 i++;
             }
         }
+
+        //Updating max and min noise value
+        float maxHeight = 0, minHeight = float.MaxValue;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            if (vertices[i].y > maxHeight) maxHeight = vertices[i].y;
+            if (vertices[i].y < minHeight) minHeight = vertices[i].y;
+        }
+
+        float heightDeltaValue = Mathf.Abs(maxHeight - minHeight);
+        //max value will now be 1, min will be 0
+        for (int i = 0; i < vertices.Length; i++) 
+            vertices[i].y = (vertices[i].y - minHeight) / heightDeltaValue;
+  
+
+        //This FL and the one above could be the same but I'm separating them for the sake of experimentation for now
+        colors = new Color[vertices.Length];
+        float waterLevel = 0.05f;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            //Using the heightMapCurve (AnimationCurve) to evaluate the height value of the mesh!
+            vertices[i].y = meshHeightCurve.Evaluate(vertices[i].y);// * heightScale;
+            if (vertices[i].y < waterLevel)
+            {
+                vertices[i].y = waterLevel;
+                colors[i] = Color.blue;
+            }
+            else if (vertices[i].y > waterLevel && vertices[i].y < 0.6f) colors[i] = Color.green;
+            else if (vertices[i].y >= 0.6f && vertices[i].y < 0.85f) colors[i] = Color.grey;
+            else colors[i] = Color.white;
+
+            vertices[i].y *= heightScale;
+        }
+
+
+        //----------------------------------------IMRPROVEMENT_TODO----------------------------------------
+
+
+        //Ordering the vertices of the mesh into triangles
         triangles = new int[xSize * zSize * 6];
         int vert = 0, tris = 0;
         for (int z = 0; z < zSize; z++)
@@ -58,6 +106,7 @@ public class MeshGenerator : MonoBehaviour
         }
    }
 
+
     /**
      * x: The world's x position.
      * z: The world's z position.
@@ -71,6 +120,7 @@ public class MeshGenerator : MonoBehaviour
      */
     float generateNoiseValue(int x, int z, float scale, int octaves, float persistance, float lucanarity, int seed, Vector2 offset)
     {
+        //Offsetting the noise value and adding different layers of random numbers to the noise 
         System.Random r = new System.Random(seed);
         Vector2[] octaveOffsets = new Vector2[octaves];
         for (int i = 0; i < octaves; i++)
@@ -88,7 +138,7 @@ public class MeshGenerator : MonoBehaviour
             float sampleX = x / scale * frequency + octaveOffsets[octave].x;
             float sampleZ = z / scale * frequency + octaveOffsets[octave].y;
 
-            float perlinValue = Mathf.PerlinNoise(sampleX * .2f, sampleZ * .2f) * 2 - 1;
+            float perlinValue = Mathf.PerlinNoise(sampleX * 0.08f, sampleZ * 0.08f) * 2;
             noiseHeight += perlinValue * amplitude;
 
             amplitude *= persistance;
@@ -104,7 +154,11 @@ public class MeshGenerator : MonoBehaviour
         
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.colors = colors;
+        
+        
 
+        //Lighting(?)
         mesh.RecalculateNormals();
 
         //Collision detection
@@ -113,7 +167,7 @@ public class MeshGenerator : MonoBehaviour
         meshCollider.sharedMesh = GetComponent<MeshFilter>().mesh;
     }
 
-    private void OnDrawGizmos()
+    /*private void OnDrawGizmos()
     {
         if (vertices == null) 
             return;
@@ -121,6 +175,6 @@ public class MeshGenerator : MonoBehaviour
         {
             Gizmos.DrawSphere(vertices[i], 0.1f);
         }
-    }
+    }*/
 
 }
