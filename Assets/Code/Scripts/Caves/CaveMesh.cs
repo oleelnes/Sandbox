3454ea9c8f322b8909484a44bdd42db778a38523
Ole Kristian Elnæs <ole.elnaes@gmail.com>
2022-10-19 11:21:08 +0200

    added caves script (with experimental code)

---
+++ Assets/Code/Scripts/Caves/CaveMesh.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class CaveMesh : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+    public int xSize = 20;
+    public int zSize = 20;
+
+    public bool createMesh = false;
+
+    void Start()
+    {
+        if (createMesh)
+        {
+            mesh = new Mesh();
+            GetComponent<MeshFilter>().mesh = mesh;
+
+            CreateShape();
+            UpdateMesh();
+        }
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                
+                vertices[i] = new Vector3(x, 1, z);
+                i++;
+            }
+        }
+        triangles = new int[xSize * zSize * 6];
+        int vert = 0, tris = 0;
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+
+
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+        //Collision detection
+        MeshCollider meshCollider = gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
+        meshCollider.sharedMesh = null;
+        meshCollider.sharedMesh = GetComponent<MeshFilter>().mesh;
+
+
+    }
+
+
+}
