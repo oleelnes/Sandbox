ffc94e61faca9adcea86a48c608bc24acaec828d
magnujg <magnujg@stud.ntnu.no>
2022-10-28 12:21:46 +0200

    made some changes to knife

--- Assets/Code/Scripts/Weapons/MeleeAttack.cs
+++ Assets/Code/Scripts/Weapons/MeleeAttack.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MeleeAttack : MonoBehaviour
 {
     //weapon animation
-    Animator anim;
+    public Animator anim;
     Collider weaponColl;
     public bool disableAnimation = false;
 
