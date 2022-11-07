786cb9f8040267379672bb1e4578370dfa66bf6a
Vayero <matti3301@gmail.com>
2022-11-07 23:26:01 +0100

    Inventory v2.1

--- Assets/Code/Scripts/inventory/FollowMousePosition.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class FollowMousePosition : MonoBehaviour {
-
-    void Update(){
-        transform.position = Input.mousePosition; 
-    }
-}
---
+++ Assets/Code/Scripts/inventory/UI Scripts/InventoryDisplay.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class InventoryDisplay : MonoBehaviour
+{
+    [SerializeField] MouseItemData mouseInventoryItem; 
+    protected InventorySystem inventorySystem; 
+    protected Dictionary<InventorySlot_UI, InventorySlot> slotDictionary;
+
+
+    public InventorySystem InventorySystem => inventorySystem;
+    public Dictionary<InventorySlot_UI, InventorySlot> SlotDictionary => slotDictionary;
+
+    protected virtual void Start() {
+
+    }
+
+    public abstract void AssignSlot(InventorySystem invToDisplay); 
+
+    protected virtual void UpdateSlot(InventorySlot updatedSlot) {
+        foreach(var slot in SlotDictionary) {
+            if(slot.Value == updatedSlot) {
+                slot.Key.UpdateUISlot(updatedSlot);
+            }
+        }
+    }
+
+    public void SlotClicked(InventorySlot_UI clickedUISlot) {
+        if(clickedUISlot.AssignedInventorySlot.ItemData != null && mouseInventoryItem.AssignedInventorySlot.ItemData == null) {
+            mouseInventoryItem.UpdateMouseSlot(clickedUISlot.AssignedInventorySlot);
+            clickedUISlot.ClearSlot();
+            return;
+        }
+        
+    }
+}
