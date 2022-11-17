c6f74b725f9a3ab86cbf5cf60759d1c65bfaf16a
Julian <30912435+Julian-MJK@users.noreply.github.com>
2022-11-17 06:13:18 +0100

    Minor: Remove inherited bool isInAttackRange

--- Assets/Code/Scripts/Animations/ChaseState.cs
+++ Assets/Code/Scripts/Animations/ChaseState.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine;  
 
 public class ChaseState : State
 {
     public AttackState attackState;
-    public bool isInAttackRange;
     public override State RunCurrentState()
     {
         if (isInAttackRange)
