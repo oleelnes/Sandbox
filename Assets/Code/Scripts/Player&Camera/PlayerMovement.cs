620b9e2807e947c0bad6a5c53109cb42761414c7
Julian <30912435+Julian-MJK@users.noreply.github.com>
2022-11-18 22:46:23 +0100

    Change: From checking input directly to being called by Actions_OnFoot input script

--- Assets/Code/Scripts/Player&Camera/PlayerMovement.cs
+++ Assets/Code/Scripts/Player&Camera/PlayerMovement.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
     [Header("Movement")]
     public float moveSpeed;
-    public bool isSprinting;
-    public bool isSuperSprinting;
+    public bool isSprinting = false;
+    public bool isSuperSprinting = false;
 
     public float groundDrag;
 
@@ -17,22 +16,26 @@ public class PlayerMovement : MonoBehaviour
     public float airMultiplier;
     public bool readyToJump;
 
+
     private float walkSpeed = 7;
     private float sprintSpeed = 14;
     private float superSprintSpeed = 7;
 
+    // Input related
+    private bool sprintKey;
     
-    [Header("Keybinds")]
-    public KeyCode jumpKey = KeyCode.Space;
-    public KeyCode sprintKey = KeyCode.LeftShift;
-    public KeyCode superSprintKey = KeyCode.Q;
-    public KeyCode crouchKey = KeyCode.LeftControl;
+    //[Header("Keybinds")]
+    //public KeyCode jumpKey = KeyCode.Space;
+    //public KeyCode sprintKey = KeyCode.LeftShift;
+    //public KeyCode superSprintKey = KeyCode.Q;
+    //public KeyCode crouchKey = KeyCode.LeftControl;
 
 
     [Header("Ground Check")]
     public float playerHeight;
-    public LayerMask whatIsGround;
+    public LayerMask whatIsGround; // baby don't hurt me
     public bool grounded;
+
 
     public Transform orientation;
 
@@ -41,14 +44,27 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 moveDirection;
 
-    Rigidbody rb;
+    public Rigidbody rb;
 
+
+
+    /*****************\
+    | Input variables |
+    \*****************/
+    private Vector2 input_MovementVec;
+
+    // TODO Could do these sfx in this file if modularity isn't a priority, or use these scripts statically
+    [SerializeField]
+    private PlayCrouchSound playCrouchSoundScript;
+    [SerializeField]
+    private PlayJumpSound playJumpSoundScript;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         readyToJump = true;
+        moveSpeed = walkSpeed;
     }
 
     private void Update()
@@ -56,21 +72,19 @@ public class PlayerMovement : MonoBehaviour
         // ground check
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
 
-        MyInput();
         SpeedControl();
-        Sprint();
-        Crouch();
-
         //Destroying/interacting with objects       
 
         // handle drag
-        if (grounded)
+        if (grounded) {
             rb.drag = groundDrag;
-        else
+        } else {   
             rb.drag = 0;
+        }
     }
 
-    private void FixedUpdate()
+
+    private void FixedUpdate() 
     {
         MovePlayer();
     }
@@ -78,35 +92,20 @@ public class PlayerMovement : MonoBehaviour
     
 
 
-    private void MyInput()
-    {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-
-        // when to jump
-        if(Input.GetKey(jumpKey) && readyToJump && grounded)
-        {
-            readyToJump = false;
-
-            Jump();
-
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
-    }
-
     private void MovePlayer()
     {
-        // calculate movement direction
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-        // on ground
-        if(grounded)
+        // Calculate movement direction
+        moveDirection = orientation.forward * input_MovementVec.y + orientation.right * input_MovementVec.x;
+        
+        // Apply different movement force depending on if the player is grounded 
+        if(grounded){
+            // on ground
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-
-        // in air
-        else if(!grounded)
-            
+        } else 
+        if(!grounded) {
+            // in air
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
     }
 
     private void SpeedControl()
@@ -121,42 +120,60 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void Jump()
-    {
-        // reset y velocity
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-    }
-
     private void ResetJump()
     {
         readyToJump = true;
     }
 
-    private void Sprint() {
-        if (Input.GetKey(sprintKey)) {
-            moveSpeed = sprintSpeed;
-            isSprinting = true;
-        } 
-        else if(Input.GetKey(superSprintKey))
-        {
-            moveSpeed = superSprintSpeed;
-            isSuperSprinting = true;
-        }
-        else {
-            moveSpeed = walkSpeed;
-            isSprinting = false;
-            isSuperSprinting = false;
+
+    
+    
+    /***************************************\
+        Methods called by Actions_OnFoot
+    \***************************************/
+    public void DoJump(){
+        if (readyToJump && grounded){
+            readyToJump = false;
+            // Reset y velocity
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+            // Add vertical force
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+            // Queue resetjump
+            Invoke(nameof(ResetJump), jumpCooldown);
+            playJumpSoundScript.playJumpAudio();
         }
     }
-
-    private void Crouch() {
-        if (Input.GetKey(crouchKey)) {
+    public void DoCrouch(){
             transform.localScale = new Vector3(0.8f, 0.5f, 0.8f);
-        } else {
-            transform.localScale = new Vector3(0.8f, 1f, 0.8f);
-        }
-
+            playCrouchSoundScript.PlayCrouchAudio();
     }
+    public void ReleaseCrouch(){
+            transform.localScale = new Vector3(0.8f, 1f, 0.8f);
+    }
+    
+    public void DoSprint(){
+        moveSpeed = sprintSpeed;
+        isSprinting = true;
+    }
+    public void ReleaseSprint(){
+        moveSpeed = walkSpeed;
+        isSprinting = false;
+        isSuperSprinting = false;
+    }
+    public void UpdateInput_Movement(Vector2 input){
+        input_MovementVec = input;
+    }
+    
+    // public void DoAttack(){
+    //     Debug.Log("DoAttack called");
+    // }
+    public void DoInteract(){
+        Debug.Log("DoInteract called");
+    }
+    // public void DoInventory(){
+    //     Debug.Log("DoInventory called");
+    // }
+
+        
+    
 }
