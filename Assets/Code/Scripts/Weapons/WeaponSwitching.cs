3d75a17a78d6cac92c41691360ae5850b356b677
Viktor Van Hulle <viktor.van.hulle@gmail.com>
2022-12-12 00:48:42 +0100

    Weapon switching ready for use; Bug: Tools anim not playing; Fix: Inputsystem?

--- Assets/Code/Scripts/Weapons/WeaponSwitching.cs
+++ Assets/Code/Scripts/Weapons/WeaponSwitching.cs
@@ -20,110 +20,38 @@ public class WeaponSwitching : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SwitchWeapon();
+        selectWeapon();
     }
 
-    private void SwitchWeapon()
+    public void SetSlot(int index)
     {
-        int previousSelectedWeaopn = selectedWeapon;
-
-        /*if (Input.GetAxis("Mouse ScrollWheel") > 0)
-        {
-            if (selectedWeapon >= transform.childCount - 1)
-            {
-                selectedWeapon = 0;
-            }
-            else
-            {
-                selectedWeapon++;
-            }
-        }
-        if (Input.GetAxis("Mouse ScrollWheel") < 0)
-        {
-            if (selectedWeapon <= 0)
-            {
-                selectedWeapon = transform.childCount - 1;
-            }
-            else
-            {
-                selectedWeapon--;
-            }
-        }*/
-
-        // CheckNumberKeyPressed();
-
-        if (previousSelectedWeaopn != selectedWeapon)
-        {
-            selectWeapon();
-        }
+        Debug.Log("Changing slot to " + index);
+        if (index >= 0 && index <= 8
+        && transform.childCount >= index + 1)
+            selectedWeapon = index;
     }
 
-    private void CheckNumberKeyPressed()
+    public void ChangeSlot_Right()
     {
-    /*    if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            selectedWeapon = 0;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
-        {
-            selectedWeapon = 1;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
-        {
-            selectedWeapon = 2;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
-        {
-            selectedWeapon = 3;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha5) && transform.childCount >= 5)
-        {
-            selectedWeapon = 4;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha6) && transform.childCount >= 6)
-        {
-            selectedWeapon = 5;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha7) && transform.childCount >= 7)
-        {
-            selectedWeapon = 6;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha8) && transform.childCount >= 8)
-        {
-            selectedWeapon = 7;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha9) && transform.childCount >= 9)
-        {
-            selectedWeapon = 8;
-        }
-		*/
+        int index = selectedWeapon + 1;
+        if (index > 8)
+            index = 0;
+
+        SetSlot(index);
     }
-	
-	public void SetSlot(int index){
-		Debug.Log("Changing slot to "+index);
-		if( index >= 0 && index <= 8 
-		&&  transform.childCount >= index+1)
-			selectedWeapon = index;
-	}
-	
-	public void ChangeSlot_Right(){
-		int index = selectedWeapon + 1;
-		if (index > 8)
-			index = 0;
-			
-		SetSlot(index);
-	}
-	public void ChangeSlot_Left(){
-		int index = selectedWeapon - 1;
-		if (index < 0)
-			index = transform.childCount; // ? -1 maybe
-		
-		SetSlot(index);
-	}
-	public void ChangeSlot(float direction){
-		if(direction < 0) ChangeSlot_Left();
-		if(direction > 0) ChangeSlot_Right();
-	}
+    public void ChangeSlot_Left()
+    {
+        int index = selectedWeapon - 1;
+        if (index < 0)
+            index = transform.childCount; // ? -1 maybe
+
+        SetSlot(index);
+    }
+    public void ChangeSlot(float direction)
+    {
+        if (direction < 0) ChangeSlot_Left();
+        if (direction > 0) ChangeSlot_Right();
+    }
 
 
     private void selectWeapon()
@@ -131,7 +59,10 @@ public class WeaponSwitching : MonoBehaviour
         int i = 0;
         foreach (Transform weapon in transform)
         {
-            weapon.gameObject.SetActive(i == selectedWeapon);
+            if (i == selectedWeapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
             i++;
         }
     }
